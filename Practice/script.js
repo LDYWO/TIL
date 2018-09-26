f65c2ce58a68a7f726e95ffbf31f546b85398547
LDYWO <ehrrnno17@naver.com>
2018-09-26 23:54:24 +0900

    20180926 Commit Start
    20180926 Commit Start

--- Practice/script.js
+++ Practice/script.js
@@ -1,15 +1,14 @@
-function A (arg) {
-    if (!(this instanceof A))
-        return new A (arg);
-    this.value = arg? arg : 0; // arg가 유의미하다면 arg, 아니면 0 할당
+function Person(name) {
+    this.name = name;
 }
 
-var a = new A (100);
-var b = A(10);
+console.log(new Person('foo')); // Person(foo)
 
-console.log(a.value);
-console.log(b.value);
-// console.log(window.value);
+function Animal(name) {
+    this.name = name;
+    return new Person('bar'); // 명시적으로 다른 객체 리턴
+}
 
-// if (!(this instanceof A)) 이 부분은 if(!(this instanceof arguments.callee))로 대체할 수 있다.
+console.log(new Animal('cat')); // Person(bar) --> Animal 객체가 아닌 명시적 값이 리턴된다.
 
+// 이 때, 생성자 함수에서 명시적으로 리턴한 값이 객체가 아닌 Boolean, 숫자, 문자열 등 기본 타입인 경우 이러한 리턴을 무시하고 this 로 바인딩된 객체가 리턴된다.
